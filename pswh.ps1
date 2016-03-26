599a38e2b4b0c2cb13bd99941677c6b8c294e59b
Bryan Lockwood <bryan@lockwood.us>
2016-03-25 23:26:19 -0700

    Resolves $1

---
+++ pswh.ps1
@@ -0,0 +1,15 @@
+﻿Function main {
+Get-OSInfo
+}
+
+function Get-OSInfo {
+<#
+.SYNOPSIS
+Returns OS version, edition and other information
+#>
+
+Get-WmiObject -Class win32_operatingsystem | select Caption, Version, OSArchitecture, ServicePackMajorVersion, ServicePackMinorVersion, WindowsDirectory, Status
+}
+
+main
+

222130f20d379803eaf9127ba6cfaf144e09c3b4
Bryan Lockwood <bryan@lockwood.us>
2016-03-25 23:58:42 -0700

    First (ugly) shot at #2

--- pswh.ps1
+++ pswh.ps1
@@ -1,5 +1,6 @@
 ﻿Function main {
 Get-OSInfo
+Get-MemoryInfo
 }
 
 function Get-OSInfo {
@@ -8,7 +9,27 @@ function Get-OSInfo {
 Returns OS version, edition and other information
 #>
 
+Write-Output "--- Version, Edition ---"
 Get-WmiObject -Class win32_operatingsystem | select Caption, Version, OSArchitecture, ServicePackMajorVersion, ServicePackMinorVersion, WindowsDirectory, Status
+}
+
+function Get-MemoryInfo {
+<#
+.SYNOPSIS
+Returns basic information about real and virtual memory.
+#>
+
+    $pfsetting = Get-WmiObject -Class win32_PageFileSetting | select Name, MaximumSize |format-table
+    Write-Output "--- Memory, Pagefile ---"
+    if ($pfsetting -eq $null) {
+        "Default system managed pagefile."
+    } else {
+        $pfsetting
+    }
+
+    gwmi win32_operatingsystem | select TotalVisibleMemorySize, 
+        SizeStoredInPagingFiles, FreeVirtualMemory, FreeSpaceInPagingFiles, 
+        FreePhysicalMemory  #needs nicer formatting.
 }
 
 main
